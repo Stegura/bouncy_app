4bd5e1f3e5cf73be50dc907562c847ab831655c2
Stegura <danylostegyra@gmail.com>
2018-07-12 23:16:18 +0300

    first commit

---
+++ app/script/scrollNav.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var scrollTop = 0;
+    $(window).scroll(function(){
+      scrollTop = $(window).scrollTop();
+       $('.counter').html(scrollTop);
+
+      if (scrollTop >= 80) {
+        $('#global-nav').addClass('scrolled-nav');
+      } else if (scrollTop < 100) {
+        $('#global-nav').removeClass('scrolled-nav');
+      }      
+      
+    }); 
+    
+  });
+
